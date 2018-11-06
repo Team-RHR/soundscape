89b32b56177f566fb32c49d2993845d7370b40a6
goebers <robert.laitila@gmail.com>
2018-11-06 14:49:03 +0200

    play_activity for the prototype

--- app/src/main/java/com/example/kkgroup/soundscape_v2/MainActivity.kt
+++ app/src/main/java/com/example/kkgroup/soundscape_v2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kkgroup.soundscape_v2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // use file from "raw"- folder for testing purposes
+        var audioFile = R.raw.muscle_car
+
+        val playIntent = Intent(this, PlayActivity::class.java)
+        playIntent.putExtra("audio", audioFile)
+
+        startActivity(playIntent)
     }
+
 }
